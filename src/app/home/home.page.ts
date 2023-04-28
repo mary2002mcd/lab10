498316fe01a273b93cbcdf8346fb5665ffbd58bd
unknown <G00401878@atu.ie>
2023-04-28 10:49:53 +0000

    last lab complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
+  long:number = 0;
+  lat:number = 0;
+  time:number = 0;
+
   constructor() {}
+
+  async getGPS(){
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    console.log('Current position:', coordinates);
+
+    this.long = coordinates.coords.longitude;
+    this.lat = coordinates.coords.latitude;
+   
+  }
   
 }
